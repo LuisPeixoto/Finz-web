b75fe59356626efe297bd512e798a3b4003f63e1
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-04 19:46:59 -0300

    feat: toast style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import GlobalStyle from './styles/global'
 import SignIn from './pages/SignIn/'
 import { AuthProvider } from './hooks/AuthContext'
+import ToasContainer from './components/ToastContainer'
 // import SignUp from './pages/SignUp/'
 
 const App: React.FunctionComponent = () => (
@@ -10,6 +11,8 @@ const App: React.FunctionComponent = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToasContainer />
 
     <GlobalStyle />
   </>
---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi'
+import { Container, Toast } from './styles'
+
+const ToasContainer: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <Toast hasDescription>
+        <FiAlertCircle size={18} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possivel fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription>
+        <FiAlertCircle size={18} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possivel fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast hasDescription={false} type="success">
+        <FiAlertCircle size={18} />
+
+        <div>
+          <strong>Aconteceu um erro</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+    </Container>
+  )
+}
+
+export default ToasContainer
