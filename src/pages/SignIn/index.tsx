502475496b5ecc9b082b9542f0623c26f2d1332f
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-05 20:03:21 -0300

    fix: toast error

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -12,7 +12,7 @@ import Input from '../../components/Input'
 import MainButton from '../../components/Button/mainButton'
 import SecondaryButton from '../../components/Button/secondaryButton'
 import getValidationErrors from '../../utils/getValidatonErrors'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import singInVector from '../../assets/sign-in-vector.svg'
 
 interface SignInFormData {
@@ -25,6 +25,7 @@ const SignIn: React.FC = () => {
 
   const { signIn } = useAuth()
   const { addToast } = useToast()
+  const history = useHistory()
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -43,17 +44,21 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         })
-      } catch (error) {
-        const errors = getValidationErrors(error)
-        formRef.current?.setErrors(errors)
 
-        return
+        history.push('/')
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error)
+          formRef.current?.setErrors(errors)
+          return
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticacão',
+          description: 'Ocorreu um erro ao fazer login, cheque as credencias',
+        })
       }
-      addToast({
-        type: 'success',
-        title: 'Erro na autenticacão',
-        description: 'Ocorreu um erro ao fazer login, cheque as credencias',
-      })
     },
     [signIn, addToast],
   )
